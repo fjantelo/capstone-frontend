2a4b57080e4a6556ebb53fd5a001c91e4b630338
Fernando Antelo <fernando@Fernandos-MacBook-Pro-2.local>
2022-08-26 14:59:14 -0400

    Adds HTML to login page and fixes link to login in template navbar

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="login">
-    <h1>This is an about page</h1>
+    <h1>Login</h1>
+    <p>Email:</p>
+    <input type="text" />
+    <p>Password:</p>
+    <input type="text" />
+    <p></p>
+    <button>Login</button>
   </div>
 </template>
+
+<script>
+// import axios from "axios";
+
+export default {};
+</script>
