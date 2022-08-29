1ca6f0a4cf3335ff2bacb156b23e9b91d24a5dc2
Fernando Antelo <fjantelo@gmail.com>
2022-08-29 14:04:03 -0400

    Adds LoginView logic to connect to backend

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="login">
-    <h1>Log in</h1>
-    <p>Email:</p>
-    <input type="text" />
-    <p>Password:</p>
-    <input type="text" />
-    <p></p>
-    <button>Log in</button>
+    <form v-on:submit.prevent="submit()">
+      <h1>Log in</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <label>Email:</label>
+      <p></p>
+      <input type="email" v-model="newSessionParams.email" />
+      <p></p>
+      <label>Password:</label>
+      <p></p>
+      <input type="password" v-model="newSessionParams.password" />
+      <p></p>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import axios from "axios";
 
-export default {};
+export default {
+  data: function () {
+    return {
+      newSessionParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      axios
+        .post("/sessions", this.newSessionParams)
+        .then((response) => {
+          axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.errors = ["Invalid email or password."];
+          this.email = "";
+          this.password = "";
+        });
+    },
+  },
+};
 </script>
