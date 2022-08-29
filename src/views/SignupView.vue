fd5a4dcbcc4a43bb66fb62b00cbb58d65d42f47f
Fernando Antelo <fjantelo@gmail.com>
2022-08-29 13:49:30 -0400

    Adds SignupView logic to connect to backend

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="signup">
     <h1>Sign up</h1>
+    <ul>
+      <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+    </ul>
     <p>Name:</p>
-    <input type="text" />
+    <input type="text" v-model="newUserParams.name" />
     <p>Email:</p>
-    <input type="text" />
+    <input type="text" v-model="newUserParams.email" />
     <p>Password:</p>
-    <input type="text" />
+    <input type="password" v-model="newUserParams.password" />
     <p>Password confirmation:</p>
-    <input type="text" />
+    <input type="password" v-model="newUserParams.password_confirmation" />
     <p></p>
-    <button>Sign up</button>
+    <button v-on:click="submit()">Sign up</button>
   </div>
 </template>
 
